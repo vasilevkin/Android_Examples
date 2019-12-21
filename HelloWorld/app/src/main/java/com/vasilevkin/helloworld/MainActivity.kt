0631ad9cde946d9f27860b09271703c84337650e
Sergey Vasilevkin <svasilev@mera.ru>
2019-12-21 21:53:49 +0300

    Android - Activities lifecycle.

--- HelloWorld/app/src/main/java/com/vasilevkin/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/vasilevkin/helloworld/MainActivity.kt
@@ -2,11 +2,49 @@ package com.vasilevkin.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
+    val msg = "Android : "
+
+    /** Called when the activity is first created. */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(msg, "The onCreate() event")
+    }
+
+    /** Called when the activity is about to become visible. */
+    override fun onStart() {
+        super.onStart()
+
+        Log.d(msg, "The onStart() event")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d(msg, "The onResume() event")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d(msg, "The onPause() event")
+    }
+
+    /** Called when the activity is no longer visible. */
+    override fun onStop() {
+        super.onStop()
+
+        Log.d(msg, "The onStop() event")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(msg, "The onDestroy() event")
     }
 }
