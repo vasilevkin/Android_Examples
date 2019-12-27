94b93e93651223d5b1a6caf6e7226d918796a31f
Sergey Vasilevkin <svasilev@mera.ru>
2019-12-27 22:35:51 +0300

    Android - Event Handling.

--- EventHandling/app/src/main/java/com/vasilevkin/eventhandling/MainActivity.kt
+++ EventHandling/app/src/main/java/com/vasilevkin/eventhandling/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.vasilevkin.eventhandling
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ProgressDialog
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var b1: Button
+    lateinit var b2: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val progress = ProgressDialog(this)
+
+        b1 = findViewById(R.id.button)
+        b2 = findViewById(R.id.button2)
+        b1.setOnClickListener {
+            val txtView = findViewById<TextView>(R.id.textView)
+            txtView.textSize = 25f
+        }
+
+        b2.setOnClickListener(object : View.OnClickListener {
+
+            override fun onClick(v: View) {
+                val txtView = findViewById(R.id.textView) as TextView
+                txtView.textSize = 55f
+            }
+        })
     }
 }
